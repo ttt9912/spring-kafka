6a591c933e70ec781166dc7dbe45eda503292816
ttt <tschachtli.th@gmail.com>
2022-01-16 16:30:54 +0100

    mtls

--- kafka-mtls/src/main/java/kafka/mtls/KafkaSslApp.java
+++ kafka-mtls/src/main/java/kafka/mtls/KafkaSslApp.java
@@ -7,6 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * - Use docker-kafka-mtls
  * - copy kafka.client.keystore.jks and kafka.client.truststore.jks
  *   from /docker-kafka-mtls to /resources/client-certs
+ *
+ *
+ * Truststore - VERIFYING the SERVER
+ *
+ * Keystore - AUTHENTICATING the CLIENT
+ * - Alternatives: SCRAM, HTTP Basic, etc.
+ *
  */
 @SpringBootApplication
 public class KafkaSslApp {
